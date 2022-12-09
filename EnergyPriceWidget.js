56c9bd22c3648da6a0a50b1510cab08b5e2b9732
Sven-Ove Bjerkan <github.com@sven-ove.no>
2022-12-09 12:23:01 +0100

    Add support for Sweden and Denmark

--- EnergyPriceWidget.js
+++ EnergyPriceWidget.js
@@ -1,21 +1,29 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: teal; icon-glyph: bolt;
+// icon-color: green; icon-glyph: bolt;
 // EnergyPriceWidget
 // v1.0.0 - first version - Sven-Ove Bjerkan
 // v1.0.1 - translated to English
+// v1.1.0 - support for Sweden and Denmark
 
-// Hvilken strømsone skal vises?
-// Gyldig verdier er: NO1, NO2, NO3, NO4 eller NO5
+// What power-zone to display?
+// NORWAY:
+// Valid values are: NO1, NO2, NO3, NO4 eller NO5
 // Sørøst-Norge, N01
 // Sørvest-Norge, N02
 // Midt-Norge, N03
 // Nord-Norge, N04
 // Vest-Norge, N05
-
 // Fortsatt usikker på hvilken sone du hører til?
 // Se kart her: https://temakart.nve.no/prosjekt/f43d4457-d6a6-41e5-82aa-df12e42aa592
-// Power-zone
+
+// SWEDEN
+// Valid values are: SE1, SE2, SE3 or SE4
+
+// DENMARK
+// Valid values are: DK1 or DK2
+
+// Power-zone - see valid settings above
 const ZONE = "NO3";
 
 // The additional cost your powercompany charges for the power, in øre. Usually 0-5 øre.
@@ -46,6 +54,41 @@ const HOURS_FORWARD = 21;
 // NO NEED TO CHANGE ANYTHING BELOW THIS!
 // --------------------------------------
 
+const COUNTRY = ZONE.slice(0,2);
+let URL, L
+if (COUNTRY == "NO") {
+  URL = "hvakosterstrommen.no";
+  L =   {
+	  ore: "øre",
+	  timepriser: "Timepriser",
+	  pris_na: "Pris nå",
+	  snitt: "Snitt",
+	  oppdatert: "Oppdatert",
+	  datakilde: "Datakilde"
+  }
+}
+else if (COUNTRY == "SE") {
+  URL = "elprisetjustnu.se";
+  L =   {
+  	  ore: "öre",
+  	  timepriser: "Timpriser",
+  	  pris_na: "Pris nu",
+  	  snitt: "Medel",
+  	  oppdatert: "Uppdaterad",
+  	  datakilde: "Datakälla"
+  }
+}
+else if (COUNTRY == "DK") {
+  URL = "elprisenligenu.dk";
+  L =   {
+	  ore: "øre",
+	  timepriser: "Timepriser",
+	  pris_na: "Pris nu",
+	  snitt: "Gennemsnit",
+	  oppdatert: "Opdateret",
+	  datakilde: "Datakilde"
+	}
+}
 
 // Date-object for the current hour
 let d = new Date();
@@ -58,19 +101,19 @@ let json, json2, json3, req;
 if (d.getHours()-HOURS_BACK < 0) {
   d2 = new Date()
   d2.setDate(d2.getDate()-1)
-  req = new Request("https://www.hvakosterstrommen.no/api/v1/prices/"+d2.getFullYear()+"/"+(d2.getMonth()+1)+"-"+pad(d2.getDate())+"_"+ZONE+".json")
+  req = new Request("https://www." + URL + "/api/v1/prices/"+d2.getFullYear()+"/"+(d2.getMonth()+1)+"-"+pad(d2.getDate())+"_"+ZONE+".json")
   json = await req.loadString()
 }
 
 // Fetch today
-req = new Request("https://www.hvakosterstrommen.no/api/v1/prices/"+d.getFullYear()+"/"+(d.getMonth()+1)+"-"+pad(d.getDate())+"_"+ZONE+".json")
+req = new Request("https://www." + URL + "/api/v1/prices/"+d.getFullYear()+"/"+(d.getMonth()+1)+"-"+pad(d.getDate())+"_"+ZONE+".json")
 json2 = await req.loadString()
 
 // Fetch tomorrow (if time is after 13:00 and HOURS_FORWARD points beyond midnight)
 if (d.getHours() >= 13 && d.getHours() + HOURS_FORWARD > 23) {
   d2 = new Date()
   d2.setDate(d2.getDate()+1)
-  req = new Request("https://www.hvakosterstrommen.no/api/v1/prices/"+d2.getFullYear()+"/"+(d2.getMonth()+1)+"-"+pad(d2.getDate())+"_"+ZONE+".json")
+  req = new Request("https://www." + URL + "/api/v1/prices/"+d2.getFullYear()+"/"+(d2.getMonth()+1)+"-"+pad(d2.getDate())+"_"+ZONE+".json")
   json3 = await req.loadString()
 }
 
@@ -123,23 +166,31 @@ d.setHours(0);
 d.setDate(d.getDate()+1)
 
 for (let i = iStart; i <= iEnd; i++) {
+  if (COUNTRY == "NO")
+    allPrices[i].per_kWh = allPrices[i].NOK_per_kWh
+ else if (COUNTRY == "SE")
+    allPrices[i].per_kWh = allPrices[i].SEK_per_kWh
+ else if (COUNTRY == "DK")
+	    allPrices[i].per_kWh = allPrices[i].DKK_per_kWh
+
+
   // Add 25% taxes if not zone=NO4
   if (ZONE != "NO4")
-  allPrices[i].NOK_per_kWh *= 1.25
+    allPrices[i].per_kWh *= 1.25
 
   // Add the power company addition
-  allPrices[i].NOK_per_kWh += (ADDITION/100)
+  allPrices[i].per_kWh += (ADDITION/100)
 
   // Convert from kroner to øre
-  allPrices[i].NOK_per_kWh *= 100
+  allPrices[i].per_kWh *= 100
 
-  avgPrice += allPrices[i].NOK_per_kWh
-  prices.push(Math.round(allPrices[i].NOK_per_kWh));
+  avgPrice += allPrices[i].per_kWh
+  prices.push(Math.round(allPrices[i].per_kWh));
 
-  if (allPrices[i].NOK_per_kWh < minPrice)
-    minPrice = Math.round(allPrices[i].NOK_per_kWh)
-  if (allPrices[i].NOK_per_kWh > maxPrice)
-    maxPrice = Math.round(allPrices[i].NOK_per_kWh)
+  if (allPrices[i].per_kWh < minPrice)
+    minPrice = Math.round(allPrices[i].per_kWh)
+  if (allPrices[i].per_kWh > maxPrice)
+    maxPrice = Math.round(allPrices[i].per_kWh)
 
   // Mark current hour in the graph
   if (i == iNow) {
@@ -180,7 +231,7 @@ url += encodeURI("{ \
       ], \
       datasets:[ \
          { \
-            label:'Øre pr kWh', \
+            label:'" + L['ore'] +"/kWh', \
             steppedLine:true, \
             data:[ \
                " + prices + " \
@@ -196,7 +247,7 @@ url += encodeURI("{ \
             ] \
          }, \
          { \
-            label:'Snitt (" + avgPrice + " øre)', \
+            label:'Snitt (" + avgPrice + " " + L['ore'] + ")', \
             data:[ \
                " + avgPrices + " \
             ], \
@@ -240,7 +291,7 @@ const GRAPH = await new Request(url).loadImage()
 
 
 // Get the price for the current hour
-let priceOre = Math.round(allPrices[iNow].NOK_per_kWh)
+let priceOre = Math.round(allPrices[iNow].per_kWh)
 
 // Create widget
 async function createWidget() {
@@ -262,7 +313,7 @@ async function createWidget() {
   lw.refreshAfterDate = d;
 
   // Add current price above the graph
-  let price = lw.addText("Pris nå: " + priceOre + " øre/kWh");
+  let price = lw.addText(L['pris_na'] + ": " + priceOre + " " + L['ore'] + "/kWh");
   price.centerAlignText();
   price.font = Font.lightSystemFont(20);
   // Define text color based on price
@@ -272,7 +323,7 @@ async function createWidget() {
     price.textColor = new Color(TEXTCOLOR_HIGH)
 
   // Add min/max price above the graph
-  let maxmin = lw.addText("Min: " + minPrice + " øre/kWh | Max: " + maxPrice + " øre/kWh")
+  let maxmin = lw.addText("Min: " + minPrice + " " + L['ore'] + "/kWh | Max: " + maxPrice + " " + L['ore'] + "/kWh")
   maxmin.centerAlignText()
   maxmin.font = Font.lightSystemFont(12);
   maxmin.textColor = new Color(TEXTCOLOR);
@@ -280,7 +331,7 @@ async function createWidget() {
   // A bit of space above graph
   lw.addSpacer(25)
 
-  let overskrift = "Timepriser";
+  let overskrift = L['timepriser'];
   if (ZONE == "NO4")
     overskrift += " (eks MVA)"
   graphTxt = lw.addText(overskrift);
@@ -307,7 +358,7 @@ async function createWidget() {
   let hour = d.getHours();
   let min = d.getMinutes();
 
-  let time = lw.addText("Oppdatert: " + pad(hour) + ":" + pad(min));
+  let time = lw.addText(L['oppdatert'] + ": " + pad(hour) + ":" + pad(min));
   time.centerAlignText();
   time.font = Font.lightSystemFont(12);
   time.textColor = new Color(TEXTCOLOR);
@@ -315,7 +366,7 @@ async function createWidget() {
   lw.addSpacer(5)
 
   // Add "link"
-  let link = lw.addText("Datakilde: www.HvaKosterStrømmen.no");
+  let link = lw.addText(L['datakilde'] + ": www." + URL);
   link.centerAlignText();
   link.font = Font.lightSystemFont(10);
   link.textColor = new Color(TEXTCOLOR);
@@ -327,17 +378,22 @@ async function createWidget() {
 let widget = await createWidget();
 
 // When clicking on the widget, it should open the website of that zone
-let by = "trondheim";
-if (ZONE == "NO1")
-  by = "oslo";
-else if (ZONE == "NO2")
-  by = "kristiansand"
-else if (ZONE == "NO4")
-  by = "tromso"
-else if (ZONE == "NO5")
-  by = "bergen"
+if (COUNTRY == "NO") {
+	let by = "trondheim";
+	if (ZONE == "NO1")
+	  by = "oslo";
+	else if (ZONE == "NO2")
+	  by = "kristiansand"
+	else if (ZONE == "NO4")
+	  by = "tromso"
+	else if (ZONE == "NO5")
+	  by = "bergen"
 
-widget.url = 'https://www.hvakosterstrommen.no/i/'+by
+	widget.url = 'https://www.' + URL + '/i/'+by
+}
+else {
+	widget.url = 'https://www.' + URL
+}
 
 function pad(n){return n<10 ? '0'+n : n}
 
